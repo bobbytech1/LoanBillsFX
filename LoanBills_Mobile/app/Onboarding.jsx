ee6f681872a517cbd0490d87c933f7ecd7b930b0
bobbytech1 <aiyevbekpendivine@gmail.com>
2024-08-11 22:00:58 +0100

    Made some new changes

--- LoanBills_Mobile/app/Onboarding.jsx
+++ LoanBills_Mobile/app/Onboarding.jsx
@@ -1,6 +1,6 @@
 import { View, Text, ScrollView, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
 import { Redirect, router } from 'expo-router';
 import { images } from '../constants'
 import CustomButton from '../components/Button/CustomButton';
@@ -8,10 +8,10 @@ import CustomButton from '../components/Button/CustomButton';
 const Onboarding = () => {
   return (
     <SafeAreaView className="bg-customblack">
-      <ScrollView contentContainerStyle={{height: '100%'}}>
-        <View className="w-full justify-center items-center" style={[ { height: hp(100) },]}>
-          <Image source={images.LogoSide} resizeMode='contain' style={[ { height: hp(12) },]}/>
-          <Image source={images.Download} resizeMode='contain' style={[ { height: hp(32) },]}/>
+      <ScrollView contentContainerStyle={{height: verticalScale(700)}}>
+        <View className="w-full justify-center items-center" style={{height: verticalScale(650)}}>
+          <Image source={images.LogoSide} resizeMode='contain' style={{height: verticalScale(80)}}/>
+          <Image source={images.Download} resizeMode='contain' style={{height: verticalScale(230)}}/>
           <View className="relative mt-5">
              <Text className="text-customwhite text-3xl font-pbold text-center">Streamline Your Finances</Text>
              <Text className="text-customwhite text-xl capitalize text-center font-pmedium">Hassle-Free Currency Exchange, Flight Booking, and Bill Payments</Text>
--- LoanBills_Mobile/app/index.jsx
+++ LoanBills_Mobile/app/index.jsx
@@ -1,6 +1,7 @@
 import { Text, View, Image, ScrollView } from 'react-native';
 import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { images } from "../constants"
 import CustomButton from '../components/Button/CustomButton';
@@ -8,9 +9,9 @@ import CustomButton from '../components/Button/CustomButton';
 export default function App() {
   return (
     <SafeAreaView className="bg-customblack" >
-      <ScrollView contentContainerStyle={{height: '100%'}}>
-        <View className="w-full justify-center items-center" style={[ { height: hp(100) },]}>
-          <Image source={images.Logo} resizeMode='contain' style={[ { height: hp(30) },]}/>
+      <ScrollView contentContainerStyle={{height: verticalScale(700)}}>
+        <View className="w-full justify-center items-center" style={{height: verticalScale(650)}}>
+          <Image source={images.Logo} resizeMode='contain' style={{height: verticalScale(200)}}/>
           <View className="relative mt-5">
              <Text className="text-customwhite text-3xl font-pbold text-center">Welcome</Text>
              <Text className="text-customwhite text-xl capitalize text-center font-pmedium">we are glad that you are here </Text>
