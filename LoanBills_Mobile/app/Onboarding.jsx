9168eb8d808372df2d9593383d1261ae144eb717
bobbytech1 <aiyevbekpendivine@gmail.com>
2024-08-11 21:20:16 +0100

    Adjusted the Dimensions in the onboarding page

--- LoanBills_Mobile/app/Onboarding.jsx
+++ LoanBills_Mobile/app/Onboarding.jsx
@@ -1,5 +1,6 @@
 import { View, Text, ScrollView, Image } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { Redirect, router } from 'expo-router';
 import { images } from '../constants'
 import CustomButton from '../components/Button/CustomButton';
@@ -8,9 +9,9 @@ const Onboarding = () => {
   return (
     <SafeAreaView className="bg-customblack h-full">
       <ScrollView contentContainerStyle={{height: '100%'}}>
-        <View className="w-full justify-center items-center min-h-[100vh]">
-          <Image source={images.LogoSide} resizeMode='contain' className="h-[10%]"/>
-          <Image source={images.Download} resizeMode='contain' className="h-[35%]"/>
+        <View className="w-full justify-center items-center" style={[ { height: hp('100%') },]}>
+          <Image source={images.LogoSide} resizeMode='contain' style={[ { height: hp('10%') },]}/>
+          <Image source={images.Download} resizeMode='contain' style={[ { height: hp('35%') },]}/>
           <View className="relative mt-5">
              <Text className="text-customwhite text-3xl font-pbold text-center">Streamline Your Finances</Text>
              <Text className="text-customwhite text-xl capitalize text-center font-pmedium">Hassle-Free Currency Exchange, Flight Booking, and Bill Payments</Text>
