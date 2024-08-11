eebd838a794c0187ab7d68c0863810aa962085b8
bobbytech1 <aiyevbekpendivine@gmail.com>
2024-08-11 21:36:11 +0100

    Made some changes to the dimensions

--- LoanBills_Mobile/app/index.jsx
+++ LoanBills_Mobile/app/index.jsx
@@ -1,6 +1,7 @@
 import { Text, View, Image, ScrollView } from 'react-native';
 import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { images } from "../constants"
 import CustomButton from '../components/Button/CustomButton';
 
@@ -8,8 +9,8 @@ export default function App() {
   return (
     <SafeAreaView className="bg-customblack h-full">
       <ScrollView contentContainerStyle={{height: '100%'}}>
-        <View className="w-full justify-center items-center min-h-[100vh] ">
-          <Image source={images.Logo} resizeMode='contain' className="h-[30%]"/>
+        <View className="w-full justify-center items-center" style={[ { height: hp(100) },]}>
+          <Image source={images.Logo} resizeMode='contain' style={[ { height: hp(30) },]}/>
           <View className="relative mt-5">
              <Text className="text-customwhite text-3xl font-pbold text-center">Welcome</Text>
              <Text className="text-customwhite text-xl capitalize text-center font-pmedium">we are glad that you are here </Text>
